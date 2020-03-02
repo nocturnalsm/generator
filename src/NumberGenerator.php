247c1b619a250941cd48b4530f059d2486432757
Bambang Sugiarto <basugi99@gmail.com>
2020-03-02 10:59:13 +0700

    stable commit

--- src/NumberGenerator.php
+++ src/NumberGenerator.php
@@ -10,49 +10,52 @@ class NumberGenerator {
     {        
         preg_match_all('/{(.*?)}/', $codeFormat, $matches);
         $patterns = Array();        
-        $digits = "";
-        $resultCode = $codeFormat;
+        $increments = "";
+        $resultCode = $codeFormat;        
         foreach ($matches[1] as $string){
             $format = "";
-            if (strpos($string,"::") > 0){
-                list($format,$str) = explode("::", $string);
+            if (strpos($string,":") > 0){
+                list($format, $str) = explode(":", $string);
             }
-            if ($format == "d"){
-                $value = $this->generateDate($str);
+            if ($format == "d"){ // date format
+                $value = self::generateDate($str);
             }
-            else if ($format == "r"){
-                $value = $this->generateRandom($str);
+            else if ($format == "r"){ // random format
+                $value = self::generateRandom($str);
             }
-            else if ($format == "n"){               
-                $digits = $str;                
+            else if ($format == "i"){ // increments
+                $increments = $str;                
             }
             else {
                 if (isset($params[$string])){                
                     $value = $params[$string];
                 }
-            }
-            if ($format != "n"){
+            }                             
+            if ($format != "" && $format != "i"){                
                 $resultCode = str_replace("{" .$string ."}", $value, $resultCode);
             }            
-        }   
-        if ($digits != ""){
+        }           
+        if ($increments != ""){
             $max = DB::table("numbers")
                     ->where("prefix", $resultCode)
-                    ->max("digit");
-            $max = str_pad(intval($max)+1, strlen($digits), substr($digits,0,1));
-            $resultCode = str_replace("{n::" .$string ."}", $max, $resultCode);
+                    ->max("increments");
+            if (!$max){
+                $max = 0;
+            }            
+            $max = str_pad(intval($max)+1, strlen($increments), substr($increments,0,1), STR_PAD_LEFT);                        
             DB::table("numbers")->insert([
                 "prefix" => $resultCode,
-                "digit" => $max
-            ]);
+                "increments" => $max
+            ]);            
+            $resultCode = str_replace("{i:" .$increments ."}", $max, $resultCode);                                    
         }        
         return $resultCode;
     }
-    private function generateDate($string)
+    private static function generateDate($string)
     {
         return Date($string);
     }
-    private function generateRandom($string)
+    private static function generateRandom($string)
     {
         $length = strlen($string);
         return mt_rand(intval(str_repeat("9", $length - 1)),intval(str_repeat("9", $length)));
--- src/NumberGeneratorProvider.php
+++ src/NumberGeneratorProvider.php
@@ -4,28 +4,39 @@ namespace NocturnalSm\NumberGenerator;
 
 use Illuminate\Support\ServiceProvider;
 use NocturnalSm\NumberGenerator\NumberGenerator;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 
 class NumberGeneratorProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
         $this->publishes([
             __DIR__.'/../database/migrations/create_numbergenerator_tables.php.stub' => $this->getMigrationFileName($filesystem),
-        ], 'migrations');     
-
-        $this->publishes([
-            __DIR__.'/../config/numbergenerator.php' => config_path('numbergenerator.php'),
-        ], 'config');
-
+        ], 'migrations');            
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/numbergenerator.php', 'numbergenerator'
-        );
         $this->app->singleton(NumberGenerator::class, function ($app) {
             return new NumberGenerator;
         });        
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_numbergenerator_table.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_numbergenerator_table.php")
+            ->first();
     }
 }
